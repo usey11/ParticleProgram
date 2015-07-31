5f38b2244e0faf8f90465b8451743fce5a93258f
usey11 <useydaninja@gmail.com>
2015-07-31 14:56:59 +0100

    First

---
+++ program.cpp
@@ -0,0 +1,78 @@
+#include "program.hpp"
+
+programLoop::programLoop(sf::RenderWindow &w)
+{
+	sf::Font arial;
+	arial.loadFromFile("arial.ttf");
+	window = &w;
+}
+
+void programLoop::loop()
+{
+  int error  = 0;
+
+  Menu menu(1,*window);
+	sf::Font font;
+	font.loadFromFile("arial.ttf");
+
+	sf::Text text("Jacob", font);
+	text.setCharacterSize(60);
+	text.setStyle(sf::Text::Bold);
+	text.setColor(sf::Color::White);
+
+	sf::FloatRect tRect = text.getLocalBounds();
+	float h = tRect.height;
+	float w = tRect.width;
+
+	text.setOrigin(w / 2, h / 2);
+	text.setPosition(400,300);
+
+  while (window->isOpen())
+  {
+	  sf::Event event;
+	  while (window->pollEvent(event))
+	  {
+		  if (event.type == sf::Event::Closed)
+			  window->close();
+	  }
+
+
+
+
+	  window->clear();
+	  menu.draw(*window);
+	  window->draw(text);
+	  window->display();
+  }
+}
+
+Menu::Menu(int a, sf::RenderWindow &w)
+	:outBox(sf::Vector2f(200, 400)), paused(sf::Vector2f(200,80))
+{
+	outBox.setOrigin(100,200);
+	outBox.setPosition(400,300);
+	outBox.setFillColor(sf::Color::Black);
+	outBox.setOutlineThickness(2);
+	outBox.setOutlineColor(sf::Color::Red);
+
+	paused.setOrigin(100, 0);
+	paused.setPosition(400, 100);
+	paused.setOutlineColor(sf::Color::Red);
+	paused.setFillColor(sf::Color::Black);
+	paused.setOutlineThickness(2);
+
+	std::cout <<a;
+	window = &w;
+}
+
+void Menu::draw(sf::RenderWindow &w)
+{
+	w.draw(outBox);
+	w.draw(paused);
+}
+
+menuItem::menuItem(std::string text, Menu &parentMenu,int items)
+{
+	windowHeight = parentMenu.window->getSize().y;
+	windowLength = parentMenu.window->getSize().x;
+}
---
+++ program.hpp
@@ -0,0 +1,50 @@
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+class programLoop
+{
+public:
+  programLoop(sf::RenderWindow &w);
+  void loop();
+  sf::Text t;
+private:
+
+  sf::RenderWindow *window;
+};
+
+class Menu
+{
+public:
+  Menu(int,sf::RenderWindow &w);
+  void draw(sf::RenderWindow &w);
+  sf::RenderWindow *window;
+private:
+  sf::RectangleShape outBox;
+  sf::RectangleShape paused;
+
+
+  //Paused
+  //Resume
+  //Remake
+  //Audio
+  //Gravity
+};
+
+class menuItem :public sf::Drawable
+{
+public:
+	menuItem(std::string, Menu&,int);
+	//active
+	
+
+private:
+	sf::RectangleShape box;
+
+	float windowHeight;
+	float windowLength;
+
+	float menuHeight;
+	float menuWidth;
+};
