159bfbf7845e3c7455478231e81eb9b8745e9ae7
usey11 <useydaninja@gmail.com>
2015-08-03 19:32:18 +0100

    1

--- Source.cpp
+++ Source.cpp
@@ -4,20 +4,12 @@
 
 int main()
 {
-
-<<<<<<< HEAD
 	sf::RenderWindow window(sf::VideoMode(1360, 768), "SFML works!",sf::Style::Fullscreen);
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 	programLoop p1(window);
 	p1.loop();
-=======
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!"); //Makes window to draw to
-	//sf::CircleShape shape(100.f);
-	//shape.setFillColor(sf::Color::Green);
-	programLoop p1(window); //Loop for program
-	p1.loop(); //Starts loop
->>>>>>> origin/master
+
 
 	return 0;
 }
